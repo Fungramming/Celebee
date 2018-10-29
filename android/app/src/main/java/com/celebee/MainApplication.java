860fb011418faaacfd43a2432b808d7c6a62c20f
Hyeonwoo Jeon <medianhw@gmail.com>
2018-10-29 12:09:43 +0900

    Chore: combine with android

--- android/app/src/main/java/com/celebee/MainApplication.java
+++ android/app/src/main/java/com/celebee/MainApplication.java
@@ -3,6 +3,7 @@ package com.celebee;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new RNGoogleSigninPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
