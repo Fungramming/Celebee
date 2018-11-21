7d60ad1aea78b4bde24da62fb7a71aea849c176e
choisho <choisho1136@gmail.com>
2018-11-21 20:46:47 +0900

    Merge: feature-navigation

--- android/app/src/main/java/com/celebee/MainActivity.java
+++ android/app/src/main/java/com/celebee/MainActivity.java
@@ -1,18 +1,19 @@
 package com.celebee;
 import android.content.Intent;
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
-
+// public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
 
-    @Override
-    protected String getMainComponentName() {
-        return "Celebee";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "Celebee";
+    // }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
--- android/app/src/main/java/com/celebee/MainApplication.java
+++ android/app/src/main/java/com/celebee/MainApplication.java
@@ -18,51 +18,102 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
 
+
+// public class MainApplication extends Application implements ReactApplication {
+
+//   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+//   protected static CallbackManager getCallbackManager() {
+//     return mCallbackManager;
+//   }
+
+//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
+
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage(),
+//             new RNKakaoLoginsPackage(),
+//             new RNGoogleSigninPackage(),
+//             new VectorIconsPackage(),
+//             new SplashScreenReactPackage(),
+//             new ImagePickerPackage(),
+//             new FBSDKPackage(mCallbackManager)
+//       );
+//     }
+
+//     @Override
+//     protected String getJSMainModuleName() {
+//       return "index";
+//     }
+//   };
+
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }
+
+public class MainApplication extends NavigationApplication {
+  
   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
   protected static CallbackManager getCallbackManager() {
     return mCallbackManager;
   }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    protected ReactGateway createReactGateway() {
+        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+        return new ReactGateway(this, isDebug(), host);
     }
 
     @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+            // eg. new VectorIconsPackage()
             new RNKakaoLoginsPackage(),
             new RNGoogleSigninPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new ImagePickerPackage(),
             new FBSDKPackage(mCallbackManager)
-      );
+        );
     }
-
+  
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
