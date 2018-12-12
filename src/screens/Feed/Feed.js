bef3e4c5ded1724e7eeaf6705c92d13a2d247889
Hyeonwoo Jeon <medianhw@gmail.com>
2018-12-12 18:41:02 +0900

    Add: add detail Screen

--- src/screens/Feed/Feed.js
+++ src/screens/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Platform, SafeAreaView, StatusBar, PixelRatio, ListView, View, Text, StyleSheet, TouchableOpacity, TextInput, DatePickerIOS, FlatList} from "react-native";
+import { Platform, SafeAreaView, StatusBar, ScrollView, Dimensions, View, Text, StyleSheet, TouchableOpacity, TextInput, DatePickerIOS, FlatList} from "react-native";
 import { connect } from 'react-redux'
 import FeedCard from '../../components/Card/FeedCard'
 
@@ -49,44 +49,52 @@ class Feed extends Component {
   render() {
     let options = { year: 'numeric', month: 'long', day: 'numeric' };
     return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar barStyle="dark-content"/>
-        <View style={styles.header}>
-          <Text style={styles.date}>{this.state.chosenDate.toLocaleDateString('ko-KR', options)}</Text>
-          <TouchableOpacity onPress={() => this._onToggleDate()}>
-            <Text style={{paddingLeft: 18, fontSize: 18, fontWeight: 'bold'}}>+</Text>
-          </TouchableOpacity>
-          <Text>월간 캘린더</Text>
-          <Text>검색</Text>
-        </View>
-        {this.state.toggleDate 
-        ? 
-          <DatePickerIOS
-          date={this.state.chosenDate}
-          mode='date'
-          locale='kor'
-          onDateChange={this.setDate}
-        />
-        : null
-        }
-        <FlatList
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          data={this.props.userInfo.follow_idol_id}
-          renderItem={({item}) => {
-              return <IdolList name={item.idol_name}></IdolList> 
-          }}
-          keyExtractor={(item, index) => index.toString()} 
-          style={
-            {
-              marginLeft: -17,
-              paddingLeft: 29, 
-              borderBottomColor: 'rgb(200, 200, 200)', 
-              borderBottomWidth: 2.5
-            }
+      <SafeAreaView>
+        <View style={styles.container}>
+          <StatusBar barStyle="dark-content"/>
+          <View style={styles.header}>
+            <Text style={styles.date} onPress={() => this._onToggleDate()}>
+              {this.state.chosenDate.toLocaleDateString('ko-KR', options)}
+              &nbsp;<Text style={{paddingLeft: 18, fontSize: 18, fontWeight: 'bold'}}>+</Text>
+            </Text>
+            <Text>월간 캘린더</Text>
+            <Text>검색</Text>
+          </View>
+          {this.state.toggleDate 
+          ? 
+            <DatePickerIOS
+            date={this.state.chosenDate}
+            mode='date'
+            locale='kor'
+            onDateChange={this.setDate}
+            style={styles.datePicker}
+          />
+          : null
           }
-        />
-        <FeedCard></FeedCard>
+          <View style={{height: 30}}>
+            <FlatList
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              data={this.props.userInfo.follow_idol_id}
+              renderItem={({item}) => {
+                  return <IdolList style={styles.idolList} name={item.idol_name}></IdolList> 
+              }}
+              keyExtractor={(item, index) => index.toString()}
+              style={
+                {
+                  // height: 0,
+                  // marginBottom: 40,
+                  paddingLeft: 29, 
+                  borderBottomColor: 'rgb(200, 200, 200)',
+                  borderBottomWidth: 2
+                }
+              }
+            />
+          </View>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <FeedCard></FeedCard>
+          </ScrollView>
+        </View>
       </SafeAreaView>
     );
   }
@@ -102,35 +110,38 @@ export default connect(mapStateToProps)(Feed)
 
 const styles = StyleSheet.create({
   container: {
-    marginLeft: 12
-    // flex: 1,
-    // justifyContent: 'center',
+    height: '100%',
+    // height: Dimensions.get('window').height,
   },
   header: {
-    paddingTop: 18,
-    paddingBottom: 18,
-    // paddingLeft: 18,
+    paddingTop: 10,
+    paddingBottom: 17,
+    paddingLeft: 12,
     flexWrap: 'wrap', 
     alignItems: 'flex-start',
     flexDirection:'row',
+    position: 'relative',
+    zIndex: 999
   },
   date: {
     fontSize: 20, 
     fontWeight: 'bold',
-    // flexDirection:'column',
   },
-  btn: {
-    backgroundColor: 'rgb(240,240,240)'
+  datePicker: {
+    height: 0,
+    position: "relative",
+    top: -107,
+    left: 0,
+    zIndex: 998,
+  },
+  idolList: {
+    height: 0,
   },
   idolName: {
     fontSize: 14,
     fontWeight: 'bold',
     color: '#878787',
     textAlign: 'center',
-    // backgroundColor: 'purple',
-    // marginTop: 14,
-    marginBottom: 12,
     marginRight: 26,
-    height: 17,
   }
 });
